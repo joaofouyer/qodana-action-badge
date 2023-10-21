a44caf3135640c8fe438e9aa246c4eb8c8867985
João Fouyer <jfouyer@gmail.com>
2023-10-21 11:04:38 -0300

    :sparkles: Commit updated readme

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,69 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import { getInput, getBooleanInput, setFailed } from '@actions/core'
+// import * as github from '@actions/github'
+import * as fs from 'fs'
+import { simpleGit, CleanOptions } from 'simple-git'
 
 export async function run(): Promise<void> {
   try {
-    const output = core.getInput('qodana-output')
-    const generateCoverage =
-      core.getInput('generate-coverage').toLowerCase() === 'true'
-    const generateQuality =
-      core.getInput('generate-quality').toLowerCase() === 'true'
-    const generateQodanaButton =
-      core.getInput('generate-qodana-button').toLowerCase() === 'true'
+    const git = simpleGit().clean(CleanOptions.FORCE)
+    const output = getInput('qodana-output')
+    const generateCoverage = getBooleanInput('generate-coverage')
+    const generateQuality = getBooleanInput('generate-quality')
+    const generateQodanaButton = getBooleanInput('generate-qodana-button')
 
     console.log(`Coverage: ${generateCoverage}`)
     console.log(`Quality: ${generateQuality}`)
     console.log(`Button: ${generateQodanaButton}`)
     console.log(`The output from the source action is ${output}`)
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+    const coverageColorArr = [
+      'e05d44',
+      'fe7d37',
+      'dfb317',
+      'a4a61d',
+      '97ca00',
+      '4c1'
+    ]
+
+    if (generateCoverage) {
+      const coverageText = output
+        .split('Total coverage:')[1]
+        .split('  ')[1]
+        .split('%')[0]
+      console.log(coverageText)
+      const coverageNumber = Number.parseInt(coverageText)
+      const color = coverageColorArr[Math.round(coverageNumber / 20)]
+      console.log(color)
+      const coverageBadge = `(https://img.shields.io/badge/coverage-${coverageText}-${color}`
+      console.log(coverageBadge)
+      let readme = fs.readFileSync('./README.md', 'utf8')
+      if (readme) {
+        console.log(readme)
+        let readmeBadge = readme.split('![Coverage Badge]')[1]
+        readmeBadge = readmeBadge.split(')')[0]
+        if (readmeBadge) {
+          readme = readme.replace(readmeBadge, coverageBadge)
+        } else {
+          const multiline = readme.split('\n')
+          multiline.splice(2, 0, coverageBadge)
+          readme = multiline.join('\n')
+        }
+        fs.writeFile('./README.md', readme, function (err) {
+          if (err) return console.log(err)
+        })
+
+        git.add('./README.md')
+        git.commit(':robot: Updating README with coverage Badge')
+        git.push()
+      }
+    }
+    // if (generateQuality) { }
+
+    // if (generateQodanaButton) { }
+
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) setFailed(error.message)
   }
 }
